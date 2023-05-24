884d47952a71e60590414b3e72b8be983b98146d
Daniela Hernandez <xdannix2626@gmail.com>
2023-05-24 12:12:45 -0700

    agregado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { useState } from "react"
 
 const Page = () => {
   const [nombre, setNombre] = useState('Daniela  Guadalupe Hernandez Guzman');
-  const [contador, SetContador] = useState(0); 
+  const [contador, SetContador] = useState(0);
+  const [edad, setEdad] = useState(0); 
+  const [mensaje, setMensaje] = useState('');
 
 function contar(){
   SetContador(contador + 1)//Va contar el numero de clics
@@ -11,6 +13,18 @@ function contar(){
 
 function limpiar(){
   SetContador(0)
+}
+
+const manejadorFormulario = (e:any) => {
+  e.preventDefault();
+console.log('Se hizo clic')
+
+if (edad >= 18) {
+ setMensaje('Es mayor de edad');
+} else {
+  setMensaje('Es menor de edad');
+}
+
 }
 
   return (
@@ -23,10 +37,32 @@ function limpiar(){
       Contar
      </button>
      <p>{contador} numero de clics</p>
+          <form
+          onSubmit={manejadorFormulario}
+          >
+      <input 
+        type="text" 
+        placeholder = "Nombre"
+        value = {nombre}
+        onChange={(event)=> setNombre(event.target.value)}
+        />
+      <input type="submit" />
+
+      <input 
+        type="number" 
+        placeholder = "Edad"
+        value = {edad}
+        onChange={(event)=> setEdad(parseInt(event.target.value))}
+        />
+
+      <input type="submit" />
+          </form>
+    
      <button
      onClick={limpiar}>
       Borrar
      </button>
+     {mensaje}
     </div>
     )
   }
